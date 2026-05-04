f8d48785244efee7128199f902512960d6471f31
saabith <e21342@eng.pdn.ac.lk>
2026-05-04 11:01:14 +0530

    make the sidebar and header for admin

--- client/src/components/admin-view/layout.jsx
+++ client/src/components/admin-view/layout.jsx
@@ -1,18 +1,25 @@
 import { Outlet } from "react-router-dom";
-import AdminSidebar from "./sidebar";
+import AdminSideBar from "./sidebar";
 import AdminHeader from "./header";
+import { useState } from "react";
+
 function AdminLayout() {
+  const [openSidebar, setOpenSidebar] = useState(false);
+  console.log("admin layout rendered", { openSidebar } );
+
   return (
-    <div>
-        <AdminSidebar />
-        <div>
-            <AdminHeader />
-            <main>
-                <Outlet />
-            </main>
-        </div>
+    <div className="flex min-h-screen w-full">
+      {/* admin sidebar */}
+      <AdminSideBar open={openSidebar} setOpen={setOpenSidebar} />
+      <div className="flex flex-1 flex-col">
+        {/* admin header */}
+        <AdminHeader setOpen={setOpenSidebar} />
+        <main className="flex-1 flex-col flex bg-muted/40 p-4 md:p-6">
+          <Outlet />
+        </main>
+      </div>
     </div>
-  )
+  );
 }
 
 export default AdminLayout;
