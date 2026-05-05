0cfee409c32a2de7713708ac68245d1e07682b63
saabith <e21342@eng.pdn.ac.lk>
2026-05-05 22:06:47 +0530

    add product image upload functionality

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cookiesParser = require('cookie-parser');
 const cors = require('cors');
 const authRouter = require("./routes/auth/auth-routes");
+const adminProductsRouter = require("./routes/admin/product-routes");
 
 // create database connection -> we can also craete a separate file for this and import it here
 mongoose.connect("mongodb://mohamedsaabith1010_db_user:saabith123@ac-rohld23-shard-00-00.62mtyzw.mongodb.net:27017,ac-rohld23-shard-00-01.62mtyzw.mongodb.net:27017,ac-rohld23-shard-00-02.62mtyzw.mongodb.net:27017/?ssl=true&replicaSet=atlas-zq6pcn-shard-0&authSource=admin&appName=Cluster1").
@@ -30,6 +31,7 @@ console.log("authRouter:", authRouter);
 app.use(cookiesParser());
 app.use(express.json());
 app.use("/api/auth", authRouter);
+app.use("/api/admin/products", adminProductsRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
