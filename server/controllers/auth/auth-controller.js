d36221fe5d8e2b91858fed39ba92190843f3f9d6
saabith <e21342@eng.pdn.ac.lk>
2026-05-06 20:08:37 +0530

    implement shopping header with menu items and user account dropdown

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -9,7 +9,7 @@ const registerUser = async (req, res) => {
   const { userName, email, password } = req.body;
 
   try {
-    // const checkUser = await User.findOne({ email });
+    const checkUser = await User.findOne({ email });
     if (checkUser)
       return res.json({
         success: false,
