d6d8fad615a30890be978a920a0b069a1967544d
saabith <e21342@eng.pdn.ac.lk>
2026-05-07 08:36:30 +0530

    create the controller and route for product filter

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth-slice";
 import AdminProductsSlice from "./admin/products-slice";
+import ShopProductsSlice from "./shop/products-slice";
 const store = configureStore({
     reducer : {
         auth : authReducer,
-        adminProducts: AdminProductsSlice
+        adminProducts: AdminProductsSlice,
+        shopProducts : ShopProductsSlice
     },
 })
 
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const cookiesParser = require('cookie-parser');
 const cors = require('cors');
 const authRouter = require("./routes/auth/auth-routes");
 const adminProductsRouter = require("./routes/admin/product-routes");
+const shopProductsRouter = require("./routes/shop/products-routes");
 
 // create database connection -> we can also craete a separate file for this and import it here
 mongoose.connect("mongodb://mohamedsaabith1010_db_user:saabith123@ac-rohld23-shard-00-00.62mtyzw.mongodb.net:27017,ac-rohld23-shard-00-01.62mtyzw.mongodb.net:27017,ac-rohld23-shard-00-02.62mtyzw.mongodb.net:27017/?ssl=true&replicaSet=atlas-zq6pcn-shard-0&authSource=admin&appName=Cluster1").
@@ -32,6 +33,7 @@ app.use(cookiesParser());
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
+app.use("/api/shop/products", shopProductsRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
