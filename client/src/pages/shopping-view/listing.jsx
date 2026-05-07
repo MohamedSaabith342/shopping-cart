4a0cf6e4981418cc7410c20439f6ff46a3c0e2a3
saabith <e21342@eng.pdn.ac.lk>
2026-05-07 11:42:55 +0530

    add filters and sorting functionality to product listing view

--- client/src/pages/shopping-view/listing.jsx
+++ client/src/pages/shopping-view/listing.jsx
@@ -1,5 +1,5 @@
 import ProductFilter from "@/components/shopping-view/filter";
-import { useEffect } from "react";
+import { useEffect,useState} from "react";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { fetchAllFilteredProducts } from "@/store/shop/products-slice";
@@ -15,28 +15,91 @@ import { Button } from "@/components/ui/button";
 import { sortOptions } from "@/config";
 import ShoppingProductTile from "@/components/shopping-view/product-tile";
 
+function createSearchParamsHelper(filterParams) {
+  const queryParams = [];
+
+  for (const [key, value] of Object.entries(filterParams)) {
+    if (Array.isArray(value) && value.length > 0) {
+      const paramValue = value.join(",");
+
+      queryParams.push(`${key}=${encodeURIComponent(paramValue)}`);
+    }
+  }
+
+  console.log(queryParams, "queryParams");
+
+  return queryParams.join("&");
+}
+
 
 function ShoppingListing() {
 
 
   const dispatch = useDispatch();
   const {productList} = useSelector((state) => state.shopProducts);
+  const [filters, setFilters] = useState({});
+  const [sort,setSort] = useState(null);
+  const [searchParams, setSearchParams] = useState();
+
+  
+
+  function handleSort(value) {  
+    console.log("Selected sort option:", value);
+    setSort(value);
+  }
+
+   function handleFilter(getSectionId, getCurrentOption) {
+    let cpyFilters = { ...filters };
+    const indexOfCurrentSection = Object.keys(cpyFilters).indexOf(getSectionId);
+
+    if (indexOfCurrentSection === -1) {
+      cpyFilters = {
+        ...cpyFilters,
+        [getSectionId]: [getCurrentOption],
+      };
+    } else {
+      const indexOfCurrentOption =
+        cpyFilters[getSectionId].indexOf(getCurrentOption);
+
+      if (indexOfCurrentOption === -1)
+        cpyFilters[getSectionId].push(getCurrentOption);
+      else cpyFilters[getSectionId].splice(indexOfCurrentOption, 1);
+    }
+
+    setFilters(cpyFilters);
+    sessionStorage.setItem("filters", JSON.stringify(cpyFilters));
+  }
+
+  useEffect(() => {
+    setSort("price-lowtohigh");
+    console.log("filters from sessionStorage:", JSON.parse(sessionStorage.getItem("filters")));
+    setFilters(JSON.parse(sessionStorage.getItem("filters")) || {});
+  }, []);
 
   useEffect(() => {
     dispatch(fetchAllFilteredProducts());
   }, [dispatch]);
 
-  console.log("productList:", productList);
 
+  useEffect(() => {
+    if (filters && Object.keys(filters).length > 0) {
+      const createQueryString = createSearchParamsHelper(filters);
+      setSearchParams(new URLSearchParams(createQueryString));
+    }
+  }, [filters]);
+
+  console.log("productList:", productList);
+  console.log("filters:", filters);
+  
   return (
     <div className="grid grid-cols-1 md:grid-cols-[200px_1fr] gap-6 p-4 md:p-6">
-      <ProductFilter />
+      <ProductFilter filters={filters} handleFilter={handleFilter} />
       <div className="bg-background w-full rounded-lg shadow-sm">
         <div className="p-4 border-b flex items-center justify-between">
           <h2 className="text-lg font-extrabold">All Products</h2>
           <div className="flex items-center gap-3">
             <span className="text-muted-foreground">
-              10 Products
+              ${productList?.length || 0} Products
             </span>
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
@@ -50,7 +113,7 @@ function ShoppingListing() {
                 </Button>
               </DropdownMenuTrigger>
               <DropdownMenuContent align="end" className="w-[200px]">
-                <DropdownMenuRadioGroup >
+                <DropdownMenuRadioGroup value={sort} onValueChange={handleSort}>
                   {sortOptions.map((sortItem) => (
                     <DropdownMenuRadioItem
                       value={sortItem.id}
