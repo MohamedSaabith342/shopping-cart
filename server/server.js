add7784cbcc0737b91c657d9f99e3385a080ee5e
saabith <e21342@eng.pdn.ac.lk>
2026-05-07 23:31:57 +0530

    add cart routes for managing cart items

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const authRouter = require("./routes/auth/auth-routes");
 const adminProductsRouter = require("./routes/admin/product-routes");
 const shopProductsRouter = require("./routes/shop/products-routes");
+const shopCartRouter = require("./routes/shop/cart-routes");
 
 // create database connection -> we can also craete a separate file for this and import it here
 mongoose.connect("mongodb://mohamedsaabith1010_db_user:saabith123@ac-rohld23-shard-00-00.62mtyzw.mongodb.net:27017,ac-rohld23-shard-00-01.62mtyzw.mongodb.net:27017,ac-rohld23-shard-00-02.62mtyzw.mongodb.net:27017/?ssl=true&replicaSet=atlas-zq6pcn-shard-0&authSource=admin&appName=Cluster1").
@@ -34,7 +35,7 @@ app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
 app.use("/api/shop/products", shopProductsRouter);
-
+app.use("/api/shop/cart", shopCartRouter);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 }); 
