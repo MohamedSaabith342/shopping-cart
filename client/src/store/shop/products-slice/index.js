aae21e49928e6ad46c39d0fdc463298d8150cf47
saabith <e21342@eng.pdn.ac.lk>
2026-05-07 12:08:45 +0530

    implement filtering and fetching of product details in the product slice

--- client/src/store/shop/products-slice/index.js
+++ client/src/store/shop/products-slice/index.js
@@ -4,14 +4,21 @@ import axios from "axios";
 const initialState = {
   isLoading: false,
   productList: [],
-  
+  productDetails: null,
 };
 
 export const fetchAllFilteredProducts = createAsyncThunk(
   "/products/fetchAllProducts",
-  async () => {
+  async ({ filterParams, sortParams }) => {
+    console.log(fetchAllFilteredProducts, "fetchAllFilteredProducts");
+
+    const query = new URLSearchParams({
+      ...filterParams,
+      sortBy: sortParams,
+    });
+
     const result = await axios.get(
-      `http://localhost:5000/api/shop/products/get?`
+      `http://localhost:5000/api/shop/products/get?${query}`
     );
 
     console.log(result);
@@ -20,7 +27,16 @@ export const fetchAllFilteredProducts = createAsyncThunk(
   }
 );
 
+export const fetchProductDetails = createAsyncThunk(
+  "/products/fetchProductDetails",
+  async (id) => {
+    const result = await axios.get(
+      `http://localhost:5000/api/shop/products/get/${id}`
+    );
 
+    return result?.data;
+  }
+);
 
 const shoppingProductSlice = createSlice({
   name: "shoppingProducts",
@@ -36,13 +52,23 @@ const shoppingProductSlice = createSlice({
         state.isLoading = true;
       })
       .addCase(fetchAllFilteredProducts.fulfilled, (state, action) => {
-        console.log(action.payload);
         state.isLoading = false;
         state.productList = action.payload.data;
       })
       .addCase(fetchAllFilteredProducts.rejected, (state, action) => {
         state.isLoading = false;
         state.productList = [];
+      })
+      .addCase(fetchProductDetails.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchProductDetails.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.productDetails = action.payload.data;
+      })
+      .addCase(fetchProductDetails.rejected, (state, action) => {
+        state.isLoading = false;
+        state.productDetails = null;
       });
   },
 });
