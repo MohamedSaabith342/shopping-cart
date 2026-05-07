25e38aaca39946ee14a067a4acb7620ccf2bb2b1
saabith <e21342@eng.pdn.ac.lk>
2026-05-07 11:49:39 +0530

    implement product filtering and sorting in the product listing API

--- server/routes/shop/products-routes.js
+++ server/routes/shop/products-routes.js
@@ -1,12 +1,16 @@
 const express = require("express");
 
 const {
-  getFilteredProducts
+  getFilteredProducts,
+  getProductDetails,
 } = require("../../controllers/shop/products-controller");
 
 const router = express.Router();
 
 router.get("/get", getFilteredProducts);
+router.get("/get/:id", getProductDetails);
 
 
 module.exports = router;
+
+
