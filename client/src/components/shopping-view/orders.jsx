8da0646c87e71dee4beb75e1d30530aa8bd68cdc
saabith <e21342@eng.pdn.ac.lk>
2026-05-16 13:04:37 +0530

    Enhance order details view and management

--- client/src/components/shopping-view/orders.jsx
+++ client/src/components/shopping-view/orders.jsx
@@ -1,8 +1,7 @@
-
+import { useEffect, useState } from "react";
 import { Button } from "../ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 import { Dialog } from "../ui/dialog";
-import { useState } from "react";
 import {
   Table,
   TableBody,
@@ -12,57 +11,102 @@ import {
   TableRow,
 } from "../ui/table";
 import ShoppingOrderDetailsView from "./order-details";
-
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getAllOrdersByUserId,
+  getOrderDetails,
+  resetOrderDetails,
+} from "@/store/shop/order-slice";
+import { Badge } from "../ui/badge";
 
 function ShoppingOrders() {
   const [openDetailsDialog, setOpenDetailsDialog] = useState(false);
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+  const { orderList, orderDetails } = useSelector(
+    (state) => state.shopOrder
+  );
 
+  function handleFetchOrderDetails(id) {
+    dispatch(getOrderDetails(id));
+  }
 
+  useEffect(() => {
+    dispatch(getAllOrdersByUserId(user?.id));
+  }, [dispatch, user?.id]);
+
+  useEffect(() => {
+    if (orderDetails) setOpenDetailsDialog(true);
+  }, [orderDetails]);
 
   return (
     <Card>
       <CardHeader>
         <CardTitle>Order History</CardTitle>
       </CardHeader>
+
       <CardContent>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Order ID</TableHead>
-              <TableHead>Order Date</TableHead>
-              <TableHead>Order Status</TableHead>
-              <TableHead>Order Price</TableHead>
-              <TableHead>
-                <span className="sr-only">Details</span>
-              </TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-                  <TableRow>
-                    <TableCell>12346</TableCell>
-                    <TableCell>27/06/2024</TableCell>
-                    <TableCell>$2500</TableCell>
-                    <TableCell>
-                      <Dialog
-                        open={openDetailsDialog}
-                        onOpenChange={() => {
-                          setOpenDetailsDialog(false);
-                         
-                        }}
-                      >
-                        <Button
-                          onClick={() =>
-                            setOpenDetailsDialog(true)
-                          }
-                        >
-                          View Details
-                        </Button>
-                        <ShoppingOrderDetailsView />
-                      </Dialog>
-                    </TableCell>
-                  </TableRow>
-          </TableBody>
-        </Table>
+        {/* ✅ ONE Dialog wraps everything */}
+        <Dialog
+          open={openDetailsDialog}
+          onOpenChange={(open) => {
+            setOpenDetailsDialog(open);
+            if (!open) dispatch(resetOrderDetails());
+          }}
+        >
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Order ID</TableHead>
+                <TableHead>Order Date</TableHead>
+                <TableHead>Order Status</TableHead>
+                <TableHead>Order Price</TableHead>
+                <TableHead />
+              </TableRow>
+            </TableHeader>
+
+            <TableBody>
+              {orderList?.map((orderItem) => (
+                <TableRow key={orderItem._id}>
+                  <TableCell>{orderItem._id}</TableCell>
+                  <TableCell>
+                    {orderItem.orderDate.split("T")[0]}
+                  </TableCell>
+
+                  <TableCell>
+                    <Badge
+                      className={`py-1 px-3 ${
+                        orderItem.orderStatus === "confirmed"
+                          ? "bg-green-500"
+                          : orderItem.orderStatus === "rejected"
+                          ? "bg-red-600"
+                          : "bg-black"
+                      }`}
+                    >
+                      {orderItem.orderStatus}
+                    </Badge>
+                  </TableCell>
+
+                  <TableCell>${orderItem.totalAmount}</TableCell>
+
+                  <TableCell>
+                    <Button
+                      onClick={(e) => {
+                        e.currentTarget.blur(); // ✅ fixes focus issue
+                        handleFetchOrderDetails(orderItem._id);
+                      }}
+                    >
+                      View Details
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+
+          {/* ✅ SINGLE dialog content */}
+          <ShoppingOrderDetailsView orderDetails={orderDetails} />
+        </Dialog>
       </CardContent>
     </Card>
   );
