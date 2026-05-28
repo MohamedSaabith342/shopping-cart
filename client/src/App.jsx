a37b6b31227fe4464a197db7908f1d88c2dc8863
saabith <e21342@eng.pdn.ac.lk>
2026-05-28 21:01:01 +0530

    Add search products page and integrate with routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,6 +23,7 @@ import { useEffect } from "react";
 import { checkAuth } from "./store/auth-slice";
 import PaypalReturnPage from "./pages/shopping-view/paypal-return";
 import PaymentSuccessPage from "./pages/shopping-view/payment-success";
+import SearchProducts from "./pages/shopping-view/search";
 
 
 function App() {
@@ -78,6 +79,7 @@ function App() {
           <Route path="checkout" element={<ShoppingCheckout />} /> 
           <Route path="paypal-return" element={<PaypalReturnPage />} />
           <Route path="payment-success" element={<PaymentSuccessPage />} />
+          <Route path="search" element={<SearchProducts />} />
         </Route>
         <Route path="/unauth-page" element={<UnauthPage />} />
         <Route path="*" element={<h1>404 Not Found</h1>}/>
