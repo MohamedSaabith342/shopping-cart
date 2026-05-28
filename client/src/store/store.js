8fefc552e12c7f13490ae8f921903f9451381fab
saabith <e21342@eng.pdn.ac.lk>
2026-05-28 19:15:03 +0530

    Add search slice for handling product search functionality

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -5,6 +5,7 @@ import ShopProductsSlice from "./shop/products-slice";
 import shopCartSlice from "./shop/cart-slice";
 import shopAddressSlice from "./shop/address-slice";
 import shopOrderSlice from "./shop/order-slice";
+import shopSearchSlice from "./shop/search-slice";
 import adminOrderSlice from "./admin/order-slice";
 const store = configureStore({
     reducer : {
@@ -15,6 +16,7 @@ const store = configureStore({
         shopCart:shopCartSlice,
         shopAddress:shopAddressSlice,
         shopOrder: shopOrderSlice,
+        shopSearch: shopSearchSlice,
     },
 })
 
