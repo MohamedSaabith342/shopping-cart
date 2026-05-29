81fffc8ca05354e2fd88f8303bf4e31441484b2d
saabith <e21342@eng.pdn.ac.lk>
2026-05-29 15:37:03 +0530

    Add common feature routes and controller

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const shopAddressRouter = require("./routes/shop/address-routes");
 const shopOrderRouter = require("./routes/shop/order-routes");
 const shopSearchRouter = require("./routes/shop/search-routes");
 const shopReviewRouter = require("./routes/shop/review-routes");
+const commonFeatureRouter = require("./routes/common/feature-routes");
 
 
 // create database connection -> we can also craete a separate file for this and import it here
@@ -48,6 +49,8 @@ app.use("/api/shop/address", shopAddressRouter);
 app.use("/api/shop/order", shopOrderRouter);
 app.use("/api/shop/search", shopSearchRouter);
 app.use("/api/shop/review", shopReviewRouter);
+app.use("/api/common/feature", commonFeatureRouter);
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 }); 
